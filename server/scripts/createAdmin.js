01697a105ff1b5a57498cfd67a5cb01582c1814a
NIRAJ CHOUHAN <chouhan.d.nick@gmail.com>
2026-06-12 17:32:09 +0530

    Fix React Router routing on Vercel

--- server/scripts/createAdmin.js
+++ server/scripts/createAdmin.js
@@ -6,8 +6,6 @@ import Admin from "../models/Admin.js";
 
 dotenv.config();
 
-console.log("MONGODB_URI:");
-console.log(process.env.MONGODB_URI);
 
 await mongoose.connect(
   process.env.MONGODB_URI
